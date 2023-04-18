274809aa522fca7d57afbe845cc35199840e53fa
Colin Bellino <git+site@colinbellino.com>
2023-04-18 17:03:32 +0200

    Update startButton bindings on resize

--- public/js/breakout_loader.mjs
+++ public/js/breakout_loader.mjs
@@ -1,7 +1,9 @@
 {
   const startButton = document.querySelector(".start-game");
+  let dialog = null;
   let gameIsRunning = false;
   let game = null;
+  let startButtonIsOn = false;
 
   let minWidth = 700;
   let minHeight = 700;
@@ -10,23 +12,35 @@
   if (document.body.hasAttribute("data-breakout-min-height"))
     minHeight = parseInt(document.body.getAttribute("data-breakout-min-height"));
 
-  // TODO: Do this on resize
-  if (startButton && window.innerWidth >= minWidth && window.innerHeight >= minHeight) {
-    const dialog = document.createElement("p");
-    dialog.setAttribute("role", "tooltip");
-    dialog.classList.add("dialog");
-    dialog.innerHTML = "How about a nice game of breakout?";
-
-    startButton.appendChild(dialog);
-    startButton.classList.add("clickable");
-    startButton.addEventListener("click", (e) => {
-      e.preventDefault();
-      start_game();
-    });
-  }
+  bindStartButton();
+  window.addEventListener("resize", bindStartButton);
 
   if (window.location.search.includes("start_game"))
     start_game();
+
+  function bindStartButton() {
+    const canStartGame = startButton && window.innerWidth >= minWidth && window.innerHeight >= minHeight;
+    if (canStartGame) {
+      if (startButtonIsOn === false) {
+        dialog = document.createElement("p");
+        dialog.setAttribute("role", "tooltip");
+        dialog.classList.add("dialog");
+        dialog.innerHTML = "How about a nice game of breakout?";
+        startButton.classList.add("clickable");
+        startButton.appendChild(dialog);
+        startButton.addEventListener("click", start_game);
+        startButtonIsOn = true;
+      }
+      return;
+    }
+
+    if (startButtonIsOn) {
+      dialog.remove();
+      startButton.classList.remove("clickable");
+      startButton.removeEventListener("click", start_game);
+      startButtonIsOn = false;
+    }
+  }
 
   function import_style(url) {
     return new Promise((resolve, reject) => {
@@ -38,7 +52,9 @@
     });
   }
 
-  async function start_game() {
+  async function start_game(e) {
+    e.preventDefault();
+
     if (gameIsRunning)
       return;
 
