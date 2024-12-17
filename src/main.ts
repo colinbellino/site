04512f1cc4beec0ad79f61b94f905292eaddeacb
Colin Bellino <git+site@colinbellino.com>
2024-12-17 23:58:57 +0100

    Clean up

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-const vsSource = `
+const vertex_shader_source = `
     #version 300 es
     layout (location=0) in vec4 position;
     layout (location=1) in vec3 color;
@@ -9,7 +9,7 @@ const vsSource = `
         vColor = color;
     }
 `;
-const fsSource = `
+const fragment_shader_source = `
     #version 300 es
     precision highp float;
 
@@ -21,87 +21,88 @@ const fsSource = `
     }
 `;
 
-function main() {
-    const canvas = document.getElementById("draw-target") as HTMLCanvasElement | null;
-    if (canvas === null) {
-        console.error("getContext");
-        return null;
-    }
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
+const COLOR_WHITE = [1, 1, 1, 1];
 
-    const gl = canvas.getContext("webgl2");
-    if (gl === null) {
-        console.error("getContext");
-        return null;
+type State = {
+    canvas?: HTMLCanvasElement;
+    gl?:     WebGL2RenderingContext;
+    program?: WebGLProgram;
+};
+const state: State = {};
+
+function assert(condition: Boolean, message: string | null = ""): asserts condition {
+    if (!condition) {
+        if (message) {
+            console.error("Assertion failed:");
+            console.error(message);
+        } else {
+            console.error("Assertion failed!");
+        }
+        debugger;
     }
+}
+
+function main() {
+    const elements = document.getElementsByTagName("canvas");
+    state.canvas = elements[0] as HTMLCanvasElement;
+    assert(state.canvas !== null, "Canvas not found");
+    state.canvas.width = window.innerWidth;
+    state.canvas.height = window.innerHeight;
+
+    const gl = state.canvas.getContext("webgl2");
+    assert(gl !== null, "Couldn't get WebGL2 context.");
 
     {
-        // Set background to solid grey
         gl.clearColor(0.25, 0.25, 0.25, 1);
 
         // Compile shaders
-        var vertexShader = gl.createShader(gl.VERTEX_SHADER);
-        if (vertexShader === null) {
-            console.error("vertexShader");
-            return;
-        }
-        gl.shaderSource(vertexShader, vsSource.trim());
-        gl.compileShader(vertexShader);
-        if (!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)) {
-            console.error(gl.getShaderInfoLog(vertexShader));
-        }
+        const vertex_shader = gl.createShader(gl.VERTEX_SHADER);
+        assert(vertex_shader !== null, "Couldn't create vertex shader.");
+        gl.shaderSource(vertex_shader, vertex_shader_source.trim());
+        gl.compileShader(vertex_shader);
+        const vertex_shader_status : GLboolean = gl.getShaderParameter(vertex_shader, gl.COMPILE_STATUS);
+        assert(vertex_shader_status, gl.getShaderInfoLog(vertex_shader));
 
-        var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
-        if (fragmentShader === null) {
-            console.error("fragmentShader");
-            return;
-        }
-        gl.shaderSource(fragmentShader, fsSource.trim());
-        gl.compileShader(fragmentShader);
-        if (!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)) {
-            console.error(gl.getShaderInfoLog(fragmentShader));
-            return;
-        }
+        const fragment_shader = gl.createShader(gl.FRAGMENT_SHADER);
+        assert(fragment_shader !== null, "Couldn't create fragment shader.");
+        gl.shaderSource(fragment_shader, fragment_shader_source.trim());
+        gl.compileShader(fragment_shader);
+        const fragment_shader_status : GLboolean = gl.getShaderParameter(fragment_shader, gl.COMPILE_STATUS);
+        assert(fragment_shader_status, gl.getShaderInfoLog(fragment_shader));
 
         // Link shaders to WebGL program
-        var program = gl.createProgram();
-        if (program === null) {
-            console.error("program");
-            return;
-        }
-        gl.attachShader(program, vertexShader);
-        gl.attachShader(program, fragmentShader);
+        const program = gl.createProgram();
+        assert(program !== null, "Couldn't create program.");
+        gl.attachShader(program, vertex_shader);
+        gl.attachShader(program, fragment_shader);
         gl.linkProgram(program);
-        if (gl.getProgramParameter(program, gl.LINK_STATUS) === null) {
-            console.error(gl.getProgramInfoLog(program));
-            return;
-        }
+        const program_status : GLboolean = gl.getProgramParameter(program, gl.LINK_STATUS);
+        assert(program_status, gl.getProgramInfoLog(program));
 
-        // Finally, activate WebGL program
         gl.useProgram(program);
+        state.program = program;
 
         // Setup Geometry
         // Create a Vertex Buffer Object (VBO) and bind two buffers to it
         // 1. positions
-        var positions = new Float32Array([
+        const positions = new Float32Array([
             -0.5, -0.5, 0.0,
             0.5, -0.5, 0.0,
             0.0, 0.5, 0.0,
         ]);
-        var positionBuffer = gl.createBuffer();
+        const positionBuffer = gl.createBuffer();
         gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
         gl.bufferData(gl.ARRAY_BUFFER, positions, gl.STATIC_DRAW);
         gl.vertexAttribPointer(0, 3, gl.FLOAT, false, 0, 0);
         gl.enableVertexAttribArray(0);
 
         // 2. colours
-        var colors = new Float32Array([
+        const colors = new Float32Array([
             1.0, 0.0, 0.0,
             0.0, 1.0, 0.0,
             0.0, 0.0, 1.0,
         ]);
-        var colorBuffer = gl.createBuffer();
+        const colorBuffer = gl.createBuffer();
         gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
         gl.bufferData(gl.ARRAY_BUFFER, colors, gl.STATIC_DRAW);
         gl.vertexAttribPointer(1, 3, gl.FLOAT, false, 0, 0);
