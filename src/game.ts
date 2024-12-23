c04bdb5ec0f25de7c8f8277a9b898efb3c83e056
Colin Bellino <git+site@colinbellino.com>
2024-12-23 07:46:35 +0100

    Camera movement & zoom

--- src/game.ts
+++ src/game.ts
@@ -95,14 +95,20 @@ const SPRITE_PASS_INSTANCE_DATA_SIZE = 24;
 const COLOR_WHITE: Color = [1, 1, 1, 1];
 const COLOR_RED:   Color = [1, 0, 0, 1];
 const COLOR_BLUE:  Color = [0, 0, 1, 1];
+const COLOR_GREEN:  Color = [0, 1, 0, 1];
+const COLOR_YELLOW:  Color = [1, 1, 0, 1];
+const COLOR_PINK:  Color = [1, 0, 1, 1];
 
 let game: Game;
 // :sprites
 const sprites: Sprite[] = [
-    // FIXME: scale should be applied after position
-    { color: COLOR_WHITE, position: [0, 32], size: [54, 54], scale: [4, 4], rotation: 0, texture_size: [54, 54], texture_position: [0, 16] },
-    { color: COLOR_WHITE, position: [0, 0],  size: [32, 32], scale: [1, 1], rotation: 0, texture_size: [16, 16], texture_position: [0, 0] },
-    { color: COLOR_WHITE, position: [32, -32], size: [32, 32], scale: [2, 2], rotation: 0, texture_size: [16, 16], texture_position: [16, 0] },
+    { color: COLOR_WHITE,  position: [0*16, 0*16], size: [16, 16], scale: [1, 1], rotation: 0, texture_size: [16, 16], texture_position: [0, 0] },
+    { color: COLOR_BLUE,   position: [1*16, 0*16], size: [16, 16], scale: [1, 1], rotation: 0, texture_size: [16, 16], texture_position: [0, 0] },
+    { color: COLOR_RED,    position: [2*16, 0*16], size: [16, 16], scale: [1, 1], rotation: 0, texture_size: [16, 16], texture_position: [0, 0] },
+    { color: COLOR_GREEN,  position: [0*16, 1*16], size: [16, 16], scale: [1, 1], rotation: 0, texture_size: [16, 16], texture_position: [0, 0] },
+    { color: COLOR_PINK,   position: [1*16, 1*16], size: [16, 16], scale: [1, 1], rotation: 0, texture_size: [16, 16], texture_position: [0, 0] },
+    { color: COLOR_YELLOW, position: [2*16, 1*16], size: [16, 16], scale: [1, 1], rotation: 0, texture_size: [16, 16], texture_position: [16, 0] },
+    { color: COLOR_WHITE,  position: [0*16+19, 2*16+19], size: [54, 54], scale: [1, 1], rotation: 0, texture_size: [54, 54], texture_position: [0, 16] },
 ];
 
 requestAnimationFrame(main);
@@ -120,6 +126,9 @@ function main() {
 
     game.inputs = inputs_init();
     renderer_update_camera_matrix_main(game.renderer.camera_main);
+
+    game.renderer.camera_main.zoom = 4;
+    game.renderer.camera_main.position = [0, 0];
 
     if (ENABLE_SPRITE_PASS) {
         game.renderer.sprite_pass = renderer_make_sprite_pass(game.renderer.gl);
@@ -144,20 +153,35 @@ function update() {
     gl.canvas.width = game.renderer.window_size[0];
     gl.canvas.height = game.renderer.window_size[1];
 
-    if (game.renderer.size_changed) {
-        renderer_update_camera_matrix_main(game.renderer.camera_main);
-    }
-
-    gl.viewport(0, 0, game.renderer.window_size[0], game.renderer.window_size[1]);
+    renderer_update_camera_matrix_main(game.renderer.camera_main);
 
     const t = sin_01(Date.now(), 1.0 / 2000);
     // sprites[1].rotation = t * 2*Math.PI;
-    sprites[1].scale[0] = 1 + t;
-    sprites[1].scale[1] = 1 + t;
+    sprites[1].scale[0] = 1 + t/5;
+    sprites[1].scale[1] = 1 + t/5;
     // sprites[2].position[1] = -32 + 64 * t;
 
     if (game.inputs.keys["p"].released) {
         game.render_active = !game.render_active;
+    }
+
+    if (game.inputs.keys["r"].down) {
+        game.renderer.camera_main.zoom = clamp(1, 16, game.renderer.camera_main.zoom + 0.1);
+    }
+    if (game.inputs.keys["f"].down) {
+        game.renderer.camera_main.zoom = clamp(1, 16, game.renderer.camera_main.zoom - 0.1);
+    }
+    if (game.inputs.keys["z"].down) {
+        game.renderer.camera_main.position[1] += 1;
+    }
+    if (game.inputs.keys["s"].down) {
+        game.renderer.camera_main.position[1] -= 1;
+    }
+    if (game.inputs.keys["q"].down) {
+        game.renderer.camera_main.position[0] += 1;
+    }
+    if (game.inputs.keys["d"].down) {
+        game.renderer.camera_main.position[0] -= 1;
     }
 
     if (game.inputs.keys["ArrowUp"].down) {
@@ -187,6 +211,8 @@ function update() {
     render: {
         if (game.render_active)  { break render; }
 
+        gl.viewport(0, 0, game.renderer.window_size[0], game.renderer.window_size[1]);
+
         gl.clearColor(0.25, 0.25, 0.25, 1);
         gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
@@ -205,12 +231,9 @@ function update() {
                 offset += 4;
 
                 let matrix = matrix4_identity();
-                const pivot = [0.5, 0.5];
-                const size = [sprite.size[0] * sprite.scale[0], sprite.size[1] * sprite.scale[1]];
-                matrix = matrix4_multiply(matrix, matrix4_make_translation(sprite.position[0], sprite.position[1], 0));
-                matrix = matrix4_multiply(matrix, matrix4_make_translation(size[0]*-pivot[0], size[1]*-pivot[1], 0));
-                matrix = matrix4_multiply(matrix, matrix4_make_translation(size[0]*2*pivot[0], size[1]*2*pivot[1], 0));
-                matrix = matrix4_multiply(matrix, matrix4_make_scale(size[0], size[1], 0));
+                matrix = matrix4_multiply(matrix4_make_scale(sprite.size[0], sprite.size[1], 0), matrix);
+                matrix = matrix4_multiply(matrix4_make_scale(sprite.scale[0], sprite.scale[1], 0), matrix);
+                matrix = matrix4_multiply(matrix4_make_translation(sprite.position[0], sprite.position[1], 0), matrix);
                 matrix = matrix4_rotate_z(matrix, sprite.rotation);
                 instance_data.set(matrix, offset);
                 offset += 16;
@@ -245,6 +268,15 @@ function update() {
     requestAnimationFrame(update);
 }
 
+const CAMERA_DEFAULT: Camera_Orthographic = {
+    zoom: 0,
+    rotation: 0,
+    position: [0, 0],
+    projection_matrix: matrix4_identity(),
+    transform_matrix: matrix4_identity(),
+    view_matrix: matrix4_identity(),
+    view_projection_matrix: matrix4_identity(),
+};
 function renderer_init(): [Renderer, true] | [null, false] {
     const canvas = document.querySelector("canvas");
     assert(canvas !== null, "Canvas not found");
@@ -262,8 +294,7 @@ function renderer_init(): [Renderer, true] | [null, false] {
 
     // @ts-ignore
     renderer.sprite_pass = {};
-    // @ts-ignore
-    renderer.camera_main = {};
+    renderer.camera_main = CAMERA_DEFAULT;
     renderer.window_size = [window.innerWidth, window.innerHeight];
 
     return [renderer, true];
@@ -430,18 +461,18 @@ function renderer_create_program(gl: WebGL2RenderingContext, vs: string, fs: str
 
 function renderer_update_camera_matrix_main(camera: Camera_Orthographic): void {
     camera.projection_matrix = matrix4_make_orthographic_projection(
-        game.renderer.window_size[0]*-0.5, game.renderer.window_size[0]*0.5,
-        game.renderer.window_size[1]*0.5, game.renderer.window_size[1]*-0.5,
-        -1,                                 +1,
+        game.renderer.window_size[0]*-0.5, game.renderer.window_size[0]*+0.5,
+        game.renderer.window_size[1]*+0.5, game.renderer.window_size[1]*-0.5,
+        -1,                                +1,
     );
 
-    // camera.transform_matrix = matrix4_identity();
+    camera.transform_matrix = matrix4_identity();
     // camera.transform_matrix *= mat4Rotate(.{ 0, 0, 1 }, camera.rotation);
-    // camera.transform_matrix *= make_scale_matrix4(.{ 1/camera.zoom, 1/camera.zoom, 0 });
-    // camera.transform_matrix *= transpose(make_translation_matrix4(make_vector3(camera.position, 0)));
+    camera.transform_matrix = matrix4_multiply(matrix4_make_scale(camera.zoom, camera.zoom, 0), camera.transform_matrix);
+    camera.transform_matrix = matrix4_multiply(matrix4_make_translation(camera.position[0], camera.position[1], 0), camera.transform_matrix);
 
-    camera.view_matrix = matrix4_identity();
-    camera.view_projection_matrix = matrix4_multiply(camera.view_matrix, camera.projection_matrix);
+    camera.view_matrix = camera.transform_matrix;
+    camera.view_projection_matrix = matrix4_multiply(camera.projection_matrix, camera.view_matrix);
 }
 
 // :inputs
@@ -615,6 +646,9 @@ function inputs_reset(inputs: Inputs) {
     // }
 }
 
+function clamp(min: number, max: number, value: number): number {
+    return Math.min(Math.max(value, min), max);
+}
 function log_matrix(matrix: Matrix4) {
     let str = "";
     for (let i = 0; i < matrix.length; i++) {
@@ -795,6 +829,91 @@ function matrix4_transpose(m: Matrix4): Matrix4 {
     result[13] = m[7];
     result[14] = m[11];
     result[15] = m[15];
+
+    return result;
+}
+function matrix_4_inverse(m: Matrix4): Matrix4 {
+    const result = matrix4_zero();
+    var m00 = m[0 * 4 + 0];
+    var m01 = m[0 * 4 + 1];
+    var m02 = m[0 * 4 + 2];
+    var m03 = m[0 * 4 + 3];
+    var m10 = m[1 * 4 + 0];
+    var m11 = m[1 * 4 + 1];
+    var m12 = m[1 * 4 + 2];
+    var m13 = m[1 * 4 + 3];
+    var m20 = m[2 * 4 + 0];
+    var m21 = m[2 * 4 + 1];
+    var m22 = m[2 * 4 + 2];
+    var m23 = m[2 * 4 + 3];
+    var m30 = m[3 * 4 + 0];
+    var m31 = m[3 * 4 + 1];
+    var m32 = m[3 * 4 + 2];
+    var m33 = m[3 * 4 + 3];
+    var tmp_0  = m22 * m33;
+    var tmp_1  = m32 * m23;
+    var tmp_2  = m12 * m33;
+    var tmp_3  = m32 * m13;
+    var tmp_4  = m12 * m23;
+    var tmp_5  = m22 * m13;
+    var tmp_6  = m02 * m33;
+    var tmp_7  = m32 * m03;
+    var tmp_8  = m02 * m23;
+    var tmp_9  = m22 * m03;
+    var tmp_10 = m02 * m13;
+    var tmp_11 = m12 * m03;
+    var tmp_12 = m20 * m31;
+    var tmp_13 = m30 * m21;
+    var tmp_14 = m10 * m31;
+    var tmp_15 = m30 * m11;
+    var tmp_16 = m10 * m21;
+    var tmp_17 = m20 * m11;
+    var tmp_18 = m00 * m31;
+    var tmp_19 = m30 * m01;
+    var tmp_20 = m00 * m21;
+    var tmp_21 = m20 * m01;
+    var tmp_22 = m00 * m11;
+    var tmp_23 = m10 * m01;
+
+    var t0 = (tmp_0 * m11 + tmp_3 * m21 + tmp_4 * m31) -
+        (tmp_1 * m11 + tmp_2 * m21 + tmp_5 * m31);
+    var t1 = (tmp_1 * m01 + tmp_6 * m21 + tmp_9 * m31) -
+        (tmp_0 * m01 + tmp_7 * m21 + tmp_8 * m31);
+    var t2 = (tmp_2 * m01 + tmp_7 * m11 + tmp_10 * m31) -
+        (tmp_3 * m01 + tmp_6 * m11 + tmp_11 * m31);
+    var t3 = (tmp_5 * m01 + tmp_8 * m11 + tmp_11 * m21) -
+        (tmp_4 * m01 + tmp_9 * m11 + tmp_10 * m21);
+
+    var d = 1.0 / (m00 * t0 + m10 * t1 + m20 * t2 + m30 * t3);
+
+    result[0] = d * t0;
+    result[1] = d * t1;
+    result[2] = d * t2;
+    result[3] = d * t3;
+    result[4] = d * ((tmp_1 * m10 + tmp_2 * m20 + tmp_5 * m30) -
+          (tmp_0 * m10 + tmp_3 * m20 + tmp_4 * m30));
+    result[5] = d * ((tmp_0 * m00 + tmp_7 * m20 + tmp_8 * m30) -
+          (tmp_1 * m00 + tmp_6 * m20 + tmp_9 * m30));
+    result[6] = d * ((tmp_3 * m00 + tmp_6 * m10 + tmp_11 * m30) -
+          (tmp_2 * m00 + tmp_7 * m10 + tmp_10 * m30));
+    result[7] = d * ((tmp_4 * m00 + tmp_9 * m10 + tmp_10 * m20) -
+          (tmp_5 * m00 + tmp_8 * m10 + tmp_11 * m20));
+    result[8] = d * ((tmp_12 * m13 + tmp_15 * m23 + tmp_16 * m33) -
+          (tmp_13 * m13 + tmp_14 * m23 + tmp_17 * m33));
+    result[9] = d * ((tmp_13 * m03 + tmp_18 * m23 + tmp_21 * m33) -
+          (tmp_12 * m03 + tmp_19 * m23 + tmp_20 * m33));
+    result[10] = d * ((tmp_14 * m03 + tmp_19 * m13 + tmp_22 * m33) -
+          (tmp_15 * m03 + tmp_18 * m13 + tmp_23 * m33));
+    result[11] = d * ((tmp_17 * m03 + tmp_20 * m13 + tmp_23 * m23) -
+          (tmp_16 * m03 + tmp_21 * m13 + tmp_22 * m23));
+    result[12] = d * ((tmp_14 * m22 + tmp_17 * m32 + tmp_13 * m12) -
+          (tmp_16 * m32 + tmp_12 * m12 + tmp_15 * m22));
+    result[13] = d * ((tmp_20 * m32 + tmp_12 * m02 + tmp_19 * m22) -
+          (tmp_18 * m22 + tmp_21 * m32 + tmp_13 * m02));
+    result[14] = d * ((tmp_18 * m12 + tmp_23 * m32 + tmp_15 * m02) -
+          (tmp_22 * m32 + tmp_14 * m02 + tmp_19 * m12));
+    result[15] = d * ((tmp_22 * m22 + tmp_16 * m02 + tmp_21 * m12) -
+          (tmp_20 * m12 + tmp_23 * m22 + tmp_17 * m02));
 
     return result;
 }
